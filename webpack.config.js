b5aa26b42e271d23d2d8c90b0d976e393697c70c
Hamdiye Tunc <whamdiyetunc@outlook.com>
2023-09-30 22:30:49 +0300

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -8,29 +8,34 @@ module.exports = {
         publicPath: '/'
     },
     devServer: {
-        contentBase: path.resolve(__dirname, ''), // ana klasörü temel al
+        contentBase: path.resolve(__dirname, ''),
         historyApiFallback: true,
         port: 3000
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
             },
             {
-                test: /\.scss$/,
-                use: ['style-loader','css-loader','sass-loader']
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
             },
             {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
+            test: /\.scss$/,
+            use: ['style-loader','css-loader','sass-loader']
+            },
+            {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+                {
+                loader: 'file-loader',
+                },
+            ],
             }
         ]
     }
+      
 }
